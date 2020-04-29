55c9bf0db4e973ecdea67a284cce47787b33f35a
Dluzen <maciej.dluzen@capgemini.com>
2020-04-29 14:11:53 +0200

    First commit

---
+++ src/test/java/com/adsb/AdvertisementBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
